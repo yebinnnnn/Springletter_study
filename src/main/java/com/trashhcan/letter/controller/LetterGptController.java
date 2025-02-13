3049cc2a7f8538872b55fe61fd1099987773a65d
yebinnnnn <prettybird1@naver.com>
2025-02-13 21:45:29 +0900

    gpt open api 내부 프롬프트로 반환받기

--- src/main/java/com/trashhcan/letter/controller/LetterGptController.java
+++ src/main/java/com/trashhcan/letter/controller/LetterGptController.java
@@ -5,11 +5,11 @@ import com.trashhcan.letter.dto.request.GPTrequestDto;
 import com.trashhcan.letter.dto.response.GPTresponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/letterbot")
@@ -24,9 +24,20 @@ public class LetterGptController {
     private RestTemplate template;
 
     @GetMapping("/chat")
-    public String chat(@RequestParam(name = "prompt")String prompt){
+    public String chat(){
+        String prompt="'쓸모 없는'으로 시작하는 가까운 친구에게 보낼만한 편지주제 하나만 반환해줘." +
+                "예시는 다음과 같아. '쓸모없는 연말선물 추천','쓸모없는 인테리어 용품 추천' 등등이야."
+                +"되도록이면 쓸모없는 다음에 오는 명사에는 취미같은 추상적인 명사가 아니었으면 좋겠고 제품같은 명사였으면 좋겠어.";
         GPTrequestDto request = new GPTrequestDto(model, prompt);
         GPTresponseDto chatGPTResponse =  template.postForObject(apiURL, request, GPTresponseDto.class);
-        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+        return Objects.requireNonNull(chatGPTResponse).getChoices().get(0).getMessage().getContent();
     }
+
+//    @PostMapping("/chatbot")
+//    public ResponseEntity<GPTresponseDto> chatBot(@RequestBody GPTpromptDto request){
+//        String prompt=
+//        GPTrequestDto request= new GPTrequestDto(model, prompt);
+//        GPTresponseDto chatGPTResponse = template.postForObject(apiURL, request, GPTresponseDto.class);
+//        return ResponseEntity.ok(chatGPTResponse);
+//    }
 }
